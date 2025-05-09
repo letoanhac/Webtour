eed15c3907ef2ad41089c278c678f46270c3f87c
trancuong3100 <cuongct989@gmail.com>
2025-05-09 08:19:02 +0700

    Push lên nhánh Cuong

---
+++ resources/views/clients/blocks/footer.blade.php
@@ -0,0 +1,149 @@
+
+<footer>
+    <div class="container">
+      <div class="footer-top">
+        <div class="footer-pannel">
+          <ul>
+            <li><b>Về iVIVU.com</b></li>
+            <li><a href="#">Chúng tôi</a></li>
+            <li><a href="#">iVIVU Blog</a></li>
+            <li><a href="#">Quy chế hoạt động</a></li>
+          </ul>
+        </div>
+        <div class="footer-pannel">
+          <ul>
+            <li><b>Thông Tin Cần Biết</b></li>
+            <li><a href="">Điều kiện & Điều khoản</a></li>
+            <li><a href="">Chính sách giá tôt</a></li>
+            <li><a href="">Câu hỏi thường gặp</a></li>
+          </ul>
+        </div>
+        <div class="footer-pannel">
+          <ul>
+            <li><b>Đối Tác & Liên kết</b></li>
+            <li><a href="">Vietnam Airlines</a></li>
+            <li><a href="">VNExpress</a></li>
+          </ul>
+        </div>
+        <div class="footer-pannel">
+          <ul>
+            <li><b>Thành viên của</b></li>
+            <li>
+              <a href="#"><img src="{{ asset('clients/img/tmg-logo.webp') }}" alt="" /></a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer-pannel" style="margin-left: auto">
+          <ul>
+            <li><b>Được chứng nhận</b></li>
+            <li>
+              <a href="#" style="margin-right: 20px"
+                ><img src="{{ asset('clients//img/bocongthuong.webp') }}" alt=""
+              /></a>
+              <a href="#"><img src="{{ asset('clients/img/iatalogo.webp') }}" alt="" /></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="wrap-awards">
+        <a href="#"
+          ><img src="{{ asset('clients/img/ico-wta-winner-shield-2021.png') }}" alt="" /><span
+            >Đại lý Du lịch Trực tuyến Hàng đầu Việt Nam</span
+          ></a
+        >
+        <a href="#"
+          ><img src="{{ asset('clients/img/ico-wta-winner-shield-2021.png') }}" alt="" /><span
+            >Đại lý Du lịch Trực tuyến Hàng đầu Việt Nam</span
+          ></a
+        >
+        <a href="#"
+          ><img src="{{ asset('clients/img/ico-wta-winner-shield-2021.png') }}" alt="" /><span
+            >Đại lý Du lịch Trực tuyến Hàng đầu Việt Nam</span
+          ></a
+        >
+      </div>
+      <div class="warp-address">
+        <span
+          >ĐKKD: 0312788481, ngày cấp: 21/05/2014, sở KHĐT Thành Phố Hồ Chí
+          Minh</span
+        >
+        <p>
+          <b><i class="fa-solid fa-location-dot"></i> HCM:</b> Tầng 2, Tòa nhà
+          Anh Đăng, 215 Nam Kỳ Khởi Nghĩa, Phường Võ Thị Sáu, Quận 3, Tp. Hồ
+          Chí Minh (Xem bản đồ)
+        </p>
+        <p>
+          <b><i class="fa-solid fa-location-dot"></i> HCM:</b> Tầng 2, Tòa nhà
+          Anh Đăng, 215 Nam Kỳ Khởi Nghĩa, Phường Võ Thị Sáu, Quận 3, Tp. Hồ
+          Chí Minh (Xem bản đồ)
+        </p>
+        <p>
+          <b><i class="fa-solid fa-location-dot"></i> HCM:</b> Tầng 2, Tòa nhà
+          Anh Đăng, 215 Nam Kỳ Khởi Nghĩa, Phường Võ Thị Sáu, Quận 3, Tp. Hồ
+          Chí Minh (Xem bản đồ)
+        </p>
+        <div class="footer2">
+          <span><b>Bạn cần trợ giúp? Hãy gọi ngay</b></span>
+          <div class="wrap-right">
+            <p>
+              <img src="./img/icon-hotline.svg" alt="" />
+              <a href="#">1900 2045</a>
+              <img src="./img/icon-time-support.svg" alt="" />
+            </p>
+            <p>
+              <img src="./img/ic_viber.svg" alt="" />
+              <a href="#"> iVIVU Viber </a>
+            </p>
+          </div>
+          <div class="wrap-appstore">
+            <span>Quét mã QRcode để tải ứng dụng iVIVU</span>
+            <div class="content-appstore">
+              <img class="qr-code" src="{{ asset('clients/img/iVIVUcode.png') }}" alt="" />
+              <a href="#">
+                <img src="{{ asset('clients/img/tai-appstore.png') }}" alt="" />
+                <img src="{{ asset('clients/img/tai-google-play.png') }}" alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-pannel">
+        <div class="fanpage-tour">
+          <span class="title-fanpage">
+            Like để cập nhật cẩm nang du lịch
+          </span>
+          <div class="fb-like">
+            <a href="#"
+              ><i class="fa-solid fa-thumbs-up" style="margin-right: 4px"></i
+              >Like 1.3M</a
+            >
+          </div>
+          <i class="fa-regular fa-envelope" style="padding-right: 5px"></i>
+          <a href="#">cuongdz105@gmail.com</a>
+        </div>
+      </div>
+      <div class="ls-social" style="margin-top: 10px">
+        <ul>
+          <li>
+            <a href="#"><img src="./img/facebook.svg" alt="" /></a>
+          </li>
+          <li>
+            <a href="#"><img src="./img/ic_zalo.svg" alt="" /></a>
+          </li>
+          <li>
+            <a href="#"><img src="./img/ic_insta.svg" alt="" /></a>
+          </li>
+          <li>
+            <a href="#"><img src="./img/tiktok.svg" alt="" /></a>
+          </li>
+          <li>
+            <a href="#"><img src="./img/youtube.svg" alt="" /></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+
+  <script src="{{ asset('clients/js/main.js') }}"></script>
+</body>
+</html>
